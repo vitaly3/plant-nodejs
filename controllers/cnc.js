df96ca363d5906941006520f4eeb4db4ec933a44
Vitalii Ushakov <vitaly.i.ushakov@gmail.com>
2020-04-23 08:43:07 +0300

    add poll to mssql

--- controllers/cnc.js
+++ controllers/cnc.js
@@ -14,38 +14,37 @@ module.exports.getAll = async function(req, res) {
   }
 }
 
-module.exports.getDepartmentAll = async function(req, res) {
-  try {
-    const connect = await mssql.connect(keys.msSQLConfig)
-    const cnc = await connect.query(`SELECT DISTINCT department_name, department_id FROM cnc order by department_name`)
-    res.status(200).json(cnc.recordsets[0]);
-    mssql.close();
-  } catch (e) {
-    errorHandler(res, e)
-    mssql.close();
-  }
+module.exports.getCncByDepartmentId = function(req, res) {
+  new mssql.ConnectionPool(keys.msSQLConfig).connect().then(
+    pool => {
+      return pool.request().query(`select * from cnc where department_id = ${req.params.id}`)
+    })
+    .then(result => {
+      let cnc = result.recordsets[0]
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.status(200).json(cnc);
+      mssql.close();
+    })
+    .catch(err => {
+      errorHandler(res, err)
+      mssql.close();
+    });
 }
 
-module.exports.getCncByDepartmentId = async function(req, res) {
-  try {
-    const connect = await mssql.connect(keys.msSQLConfig)
-    const cnc = await connect.query(`select * from cnc where department_id = ${req.params.id}`)
-    res.status(200).json(cnc.recordsets[0]);
-    mssql.close();
-  } catch (e) {
-    errorHandler(res, e)
-    mssql.close();
-  }
-}
-
-module.exports.getDepartmentAll = async function(req, res) {
-  try {
-    const connect = await mssql.connect(keys.msSQLConfig)
-    const cnc = await connect.query(`SELECT DISTINCT department_name, department_id FROM cnc order by department_name`)
-    res.status(200).json(cnc.recordsets[0]);
-    mssql.close();
-  } catch (e) {
-    errorHandler(res, e)
-    mssql.close();
-  }
+module.exports.getDepartmentAll = function(req, res) {
+  new mssql.ConnectionPool(keys.msSQLConfig).connect().then(
+    pool => {
+      return pool.request().query(`SELECT DISTINCT department_name, department_id FROM cnc order by department_name`)
+    })
+    .then(result => {
+      console.log(result);
+      let cnc = result.recordsets[0]
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.status(200).json(cnc);
+      mssql.close();
+    })
+    .catch(err => {
+      errorHandler(res, err)
+      mssql.close();
+    });
 }
